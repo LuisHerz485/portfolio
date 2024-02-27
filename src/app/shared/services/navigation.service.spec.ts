0979769abebf813bcbc2a961b9ca179e1809b19e
Luis Hernandez <luis.hernandez.albia@gmail.com>
2024-02-27 01:49:06 -0500

    :sparkles: Feature: Tap to top

--- src/app/shared/services/navigation.service.spec.ts
+++ src/app/shared/services/navigation.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { NavigationService } from './navigation.service';
 
 describe('NavigationService', () => {
-  let service: NavigationService;
+    let service: NavigationService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NavigationService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(NavigationService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
