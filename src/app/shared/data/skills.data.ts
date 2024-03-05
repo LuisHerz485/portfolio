097e61679c6ded2396e72b43dd399e5f376cf743
Luis Hernandez <luis.hernandez.albia@gmail.com>
2024-03-05 16:00:50 -0500

    Fix: Correccion imagenes

--- src/app/shared/data/skills.data.ts
+++ src/app/shared/data/skills.data.ts
@@ -4,82 +4,82 @@ export const skill: IInfoSkills = {
     basic: {
         html: {
             titulo: 'HTML5',
-            icono: '../../assets/icons/skills/html.svg',
+            icono: 'assets/icons/skills/html.svg',
         },
         css: {
             titulo: 'CSS',
-            icono: '../../assets/icons/skills/css.svg',
+            icono: 'assets/icons/skills/css.svg',
         },
         sass: {
             titulo: 'Sass',
-            icono: '../../assets/icons/skills/sass.svg',
+            icono: 'assets/icons/skills/sass.svg',
         },
     },
     lenguajes: {
         java: {
             titulo: 'Java',
             version: ['8'],
-            icono: '../../assets/icons/skills/java.svg',
+            icono: 'assets/icons/skills/java.svg',
         },
         typeScript: {
             titulo: 'TypeScript',
-            icono: '../../assets/icons/skills/ts.svg',
+            icono: 'assets/icons/skills/ts.svg',
         },
         javaScript: {
             titulo: 'JavaScript',
-            icono: '../../assets/icons/skills/js.svg',
+            icono: 'assets/icons/skills/js.svg',
         },
         php: {
             titulo: 'PHP',
-            icono: '../../assets/icons/skills/php.svg',
+            icono: 'assets/icons/skills/php.svg',
         },
     },
     tools: {
         wordpress: {
             titulo: 'Wordpress',
-            icono: '../../assets/icons/skills/wordpress.svg',
+            icono: 'assets/icons/skills/wordpress.svg',
         },
         aws: {
             titulo: 'AWS',
             plugins: ['Codecommit'],
-            icono: '../../assets/icons/skills/aws.svg',
+            icono: 'assets/icons/skills/aws.svg',
         },
         jwt: {
             titulo: 'JWT',
-            icono: '../../assets/icons/skills/jwt.svg',
+            icono: 'assets/icons/skills/jwt.svg',
         },
         rxjs: {
             titulo: 'RxJs',
-            icono: '../../assets/icons/skills/rxjs.png',
+            icono: 'assets/icons/skills/rxjs.png',
         },
         tailwind: {
             titulo: 'Tailwind',
-            icono: '../../assets/icons/skills/tailwind.svg',
+            icono: 'assets/icons/skills/tailwind.svg',
         },
 
         bootstrap: {
             titulo: 'Bootstrap',
-            icono: '../../assets/icons/skills/bootstrap.svg',
+            icono: 'assets/icons/skills/bootstrap.svg',
         },
         npm: {
             titulo: 'NPM',
-            icono: '../../assets/icons/skills/npm.svg',
+            icono: 'assets/icons/skills/npm.svg',
         },
         swagger: {
             titulo: 'Swagger',
-            icono: '../../assets/icons/skills/swagger.svg',
+            icono: 'assets/icons/skills/swagger.svg',
         },
         leaflet: {
             titulo: 'Leaflet',
-            icono: '../../assets/icons/skills/leaflet.png',
+            icono: 'assets/icons/skills/leaflet.png',
         },
         socketio: {
             titulo: 'Socket.io',
-            icono: '../../assets/icons/skills/socketio.png',
+            icono: 'assets/icons/skills/socketio.png',
         },
         jquery: {
             titulo: 'JQuery',
-            icono: '../../assets/icons/skills/jquery.svg',
+            icono: 'assets/icons/skills/jquery.svg',
         },
     },
     frameworks: {
@@ -87,80 +87,80 @@ export const skill: IInfoSkills = {
             titulo: 'Angular',
             version: ['13', '14', '15', '16', '17'],
             plugins: ['Angular Material'],
-            icono: '../../assets/icons/skills/angular.svg',
+            icono: 'assets/icons/skills/angular.svg',
         },
         nodejs: {
             titulo: 'NodeJs',
-            icono: '../../assets/icons/skills/nodejs.svg',
+            icono: 'assets/icons/skills/nodejs.svg',
         },
         react: {
             titulo: 'ReactJs',
-            icono: '../../assets/icons/skills/react.svg',
+            icono: 'assets/icons/skills/react.svg',
         },
         springboot: {
             titulo: 'Springboot',
-            icono: '../../assets/icons/skills/springboot.svg',
+            icono: 'assets/icons/skills/springboot.svg',
         },
         express: {
             titulo: 'ExpressJS',
-            icono: '../../assets/icons/skills/express.svg',
+            icono: 'assets/icons/skills/express.svg',
         },
     },
     bd: {
         mongodb: {
             titulo: 'MongoDB',
-            icono: '../../assets/icons/skills/mongodb.svg',
+            icono: 'assets/icons/skills/mongodb.svg',
         },
         mysql: {
             titulo: 'MySQL',
-            icono: '../../assets/icons/skills/mysql.svg',
+            icono: 'assets/icons/skills/mysql.svg',
         },
         oracledb: {
             titulo: 'OracleDB',
             plugins: ['Oracle Spatial'],
             version: ['19c'],
-            icono: '../../assets/icons/skills/oracle.svg',
+            icono: 'assets/icons/skills/oracle.svg',
         },
         postgres: {
             titulo: 'Postgres',
             plugins: ['PostGIS'],
-            icono: '../../assets/icons/skills/postgres.svg',
+            icono: 'assets/icons/skills/postgres.svg',
         },
     },
     versionable: {
         git: {
             titulo: 'Git',
-            icono: '../../assets/icons/skills/git.svg',
+            icono: 'assets/icons/skills/git.svg',
         },
         github: {
             titulo: 'GitHub',
-            icono: '../../assets/icons/skills/github.svg',
+            icono: 'assets/icons/skills/github.svg',
         },
         gitlab: {
             titulo: 'GitLab',
-            icono: '../../assets/icons/skills/gitlab.svg',
+            icono: 'assets/icons/skills/gitlab.svg',
         },
     },
     metology: {
         scrum: {
             titulo: 'SCRUM',
-            icono: '../../assets/icons/skills/scrum.svg',
+            icono: 'assets/icons/skills/scrum.svg',
         },
     },
     geo: {
         arcgis: {
             titulo: 'ArcGIS',
-            icono: '../../assets/icons/skills/arcgis.png',
+            icono: 'assets/icons/skills/arcgis.png',
         },
         qgis: {
             titulo: 'QGIS',
-            icono: '../../assets/icons/skills/qgis.svg',
+            icono: 'assets/icons/skills/qgis.svg',
         },
     },
     uiux: {
         photoshop: {
             titulo: 'Photoshop',
-            icono: '../../assets/icons/skills/photoshop.svg',
+            icono: 'assets/icons/skills/photoshop.svg',
         },
     },
 };
