c01e8c7a0d72e1fc54118d929f004ea208b36545
Luis Hernandez <luis.hernandez.albia@gmail.com>
2024-03-05 16:03:13 -0500

    Fix: Correcion imagenes estudios

--- src/app/shared/data/estudios.data.ts
+++ src/app/shared/data/estudios.data.ts
@@ -3,21 +3,21 @@ import * as enumKeys from '../../shared/enums';
 
 export const estudios: IEstudio[] = [
     {
-        logo: '../../assets/img/upn.png',
+        logo: 'assets/img/upn.png',
         institucion: enumKeys.estudiosKeyTranslate.ENTIDADUPN,
         carrera: enumKeys.estudiosKeyTranslate.MAESTRIATITULO,
         periodo: enumKeys.estudiosKeyTranslate.MAESTRIAPERIODO,
         skills: [enumKeys.estudiosKeyTranslate.MAESTRIASKILLS],
     },
     {
-        logo: '../../assets/img/upn.png',
+        logo: 'assets/img/upn.png',
         institucion: enumKeys.estudiosKeyTranslate.ENTIDADUPN,
         carrera: enumKeys.estudiosKeyTranslate.DIPLOMADOTITULO,
         periodo: enumKeys.estudiosKeyTranslate.DIPLOMADOPERIODO,
         skills: [enumKeys.estudiosKeyTranslate.DIPLOMADOSKILLS],
     },
     {
-        logo: '../../assets/img/upn.png',
+        logo: 'assets/img/upn.png',
         institucion: enumKeys.estudiosKeyTranslate.ENTIDADUPN,
         carrera: enumKeys.estudiosKeyTranslate.BACHILLERTITULO,
         periodo: enumKeys.estudiosKeyTranslate.BACHILLERPERIODO,
